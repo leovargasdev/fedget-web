33e0007551f12080a36acacecbecbf9e2a1265e0
Leonardo Vargas <leu1607@hotmail.com>
2022-05-02 22:30:01 -0300

    alterando o código do componente Widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,16 +1,12 @@
 import { Popover } from '@headlessui/react'
 import { ChatTeardropDots  } from 'phosphor-react'
 
-const styles = {
-  button: 'bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'
-}
-
 export function Widget() {
   return (
     <Popover className='absolute top-4 right-4'>
       <Popover.Panel>olar mundo</Popover.Panel>
 
-      <Popover.Button className={styles.button}>
+      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
         
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
